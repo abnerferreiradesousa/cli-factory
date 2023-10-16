49c80adbbe728f5c6b66a70e3d6274706ae6d64c
Abner <abnerferreira84@gmail.com>
2023-10-16 07:55:47 -0300

    feat: add começo e fimd o programa.

--- src/com/factory/Factory.java
+++ src/com/factory/Factory.java
@@ -13,6 +13,12 @@ public class Factory {
 
     FactoryInterativeCli factoryInterativeCli = new FactoryInterativeCli();
     try (Scanner sc = new Scanner(System.in)) {
+      System.out
+          .println("========================================================================");
+      System.out
+          .println("####################### BEM-VINDO AO CLI FACTORY #######################");
+      System.out
+          .println("========================================================================");
       while (continueMenu) {
         System.out.println("[1] - Registrar uma nova ordem de produção");
         System.out.println("[2] - Listar todas as ordens de produção");
@@ -50,6 +56,13 @@ public class Factory {
             break;
         }
       }
+
+      System.out
+          .println("========================================================================");
+      System.out
+          .println("+++++++++++++++++++++++++++++++++ FIM ++++++++++++++++++++++++++++++++++");
+      System.out
+          .println("========================================================================");
     } catch (Exception ex) {
       System.out.println("Erro: " + ex);
       System.out.println("Por favor, reinicie o programa.");
